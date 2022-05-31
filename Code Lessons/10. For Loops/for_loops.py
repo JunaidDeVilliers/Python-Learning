b48ee17e121fe033d12bd0ac41987b7c798d4bc1
Junaid <junaiddevilliers@gmail.com>
2022-05-31 16:50:00 +0200

    for loops

---
+++ Code Lessons/10. For Loops/for_loops.py
@@ -0,0 +1,9 @@
+# a for loop is a statement that will execute it's bloclk of code a limited amount of times
+
+# for num in range(10, 20, 2):
+#     print(num)
+
+name = "Junaid"
+
+for letter in name:
+    print(letter)
