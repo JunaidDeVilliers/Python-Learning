26473bac05fd0f146e9c3ac1701e180458bf76a9
Junaid <junaiddevilliers@gmail.com>
2022-06-03 08:36:06 +0200

    lists

---
+++ Python 201/Resources/Personal Notes/lists1.py
@@ -0,0 +1,8 @@
+bucket_list = ["Travel", "Become Rich", "Buy a Jet", "Move to Europe", "Buy a House", "Buy a Car"]
+
+#print(bucket_list[-1])
+
+# print(bucket_list[::2])
+
+for item in bucket_list:
+    print(item)
