a9c5a48ce7f9015c6388bab7726bc0c9f84582ec
Junaid <junaiddevilliers@gmail.com>
2022-06-12 04:32:01 +0200

    file project

--- Python 201/python-201-Labs/projects/file_counter.py
+++ Python 201/python-201-Labs/projects/file_counter.py
@@ -1,1 +1,28 @@
-# Add the code for the file counter script that you wrote in the course.
+import pathlib
+import os
+
+path = pathlib.Path("/home/junaid/Desktop")
+
+my_suffix = {}
+
+new_path = pathlib.Path('/home/junaid/Desktop/images')
+new_path.mkdir(exist_ok=True)
+
+for filepath in path.iterdir():
+    if os.path.isdir(filepath):
+        continue
+    else:
+        if filepath.suffix in my_suffix:
+            new_path = pathlib.Path(f'/home/junaid/Desktop/images/({filepath.suffix})')
+            new_path.mkdir(exist_ok=True)
+            new_filepath = new_path.joinpath(filepath.name)
+            filepath.replace(new_filepath)
+            my_suffix[filepath.suffix] += 1
+        else:
+            new_path = pathlib.Path(f'/home/junaid/Desktop/images/({filepath.suffix})')
+            new_path.mkdir(exist_ok=True)
+            new_filepath = new_path.joinpath(filepath.name)
+            filepath.replace(new_filepath)
+            my_suffix[filepath.suffix] = 1
+
+print(my_suffix)
