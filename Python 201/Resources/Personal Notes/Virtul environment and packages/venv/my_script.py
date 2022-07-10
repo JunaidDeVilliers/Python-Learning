f568c4b376b39068b2e7a585502c89276ab0be99
Junaid <junaiddevilliers@gmail.com>
2022-07-10 12:45:59 +0200

    labs

--- Python 201/Resources/Personal Notes/Virtul environment and packages/venv/my_script.py
+++ Python 201/Resources/Personal Notes/Virtul environment and packages/venv/my_script.py
@@ -2,3 +2,4 @@ import os
 
 secret = os.environ['MY_SUPER_SECRET_SECRET']
 print(secret)
+
---
+++ Python 201/python-201-Labs/05_venvs-and-packages/05_02_two_versions/venv2/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/junaid/Desktop/Python/Python 201/python-201-Labs/05_venvs-and-packages/05_02_two_versions/venv2/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
